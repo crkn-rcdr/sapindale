59c09d2435f21c7416479b5d86ed31f29839814b
DivyaKrishnan26 <dkrishnan@crkn.ca>
2019-07-30 12:26:52 -0400

    Changes modified

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,15 @@
-import {get,readable} from 'svelte/store';
+import { get, readable } from "svelte/store";
 var value;
-export const state =  readable(value,function auth(set){
-fetch("https://auth.canadiana.ca/1/auth",{
-credentials:"include",
-})
-.then(function(response, data){
-    if(response.status == 200){
-       set(value = response.status);
+export const state = readable(value, function auth(set) {
+  fetch("https://auth.canadiana.ca/1/auth", {
+    credentials: "include"
+  }).then(function(response, data) {
+    if (response.status == 200) {
+      value = "SUCCESS";
+      set(value);
+    } else if (response.status == 401) {
+      value = "FAILED";
+      set(value);
     }
-    else if(response.status == 401){
-        
-        set(value = response.status);
-    }
-}) 
-}); 
+  });
+});
