7160d717836cc79c443eba9db2b18f1b7cdef738
DivyaKrishnan26 <dkrishnan@crkn.ca>
2019-08-06 12:05:56 -0400

    Futon Link added in tools index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,18 @@
 <script>
-
+  import { state } from "../auth.js";
+  var value;
+  state.subscribe(updateState => {
+    if ($state.status === "SUCCESS") {
+      name = $state.name;
+      value = $state.token;
+    }
+  });
 </script>
 
 <style>
-
+  p {
+    font-weight: bold;
+  }
 </style>
 
 <svelte:head>
@@ -14,10 +23,22 @@
 
 <h2>Tools</h2>
 <ul>
-  <li>
-    <a href="/deposit">Deposit from preservation</a>
-  </li>
-  <li>
-    <a href="/collection">Collection manager</a>
-  </li>
+  {#if $state.status === 'SUCCESS'}
+    <li>
+      <a href="/deposit">Deposit from preservation</a>
+    </li>
+    <li>
+      <a href="/collection">Collection manager</a>
+    </li>
+    <li>
+      <a
+        href="https://upholstery.canadiana.ca/cookie?token={value}"
+        id="Futon"
+        target="_blank">
+        Futon
+      </a>
+    </li>
+  {:else}
+    <p>Please Login to Access More Tools</p>
+  {/if}
 </ul>
