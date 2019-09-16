da9b0de66efa2947584737cfee46a8c13c4d1f58
DivyaKrishnan26 <dkrishnan@crkn.ca>
2019-09-16 09:29:18 -0400

    Getting the selected key value

--- src/components/CouchView.svelte
+++ src/components/CouchView.svelte
@@ -1,15 +1,16 @@
 <script>
   import { onMount } from "svelte";
   import { state as authState } from "../auth.js";
-  import { view } from "../couch.js";
+  import { view, all_docs } from "../couch.js";
 
   export let dbname, ddoc, viewname, options;
   export let viewContents = [];
-
+  export let build = [];
   export let reduce = false;
   export let startkey;
   export let endkey;
-  export let pathname;
+  export let selectedVal;
+
   $: token = $authState.token;
 
   onMount(async () => {
@@ -20,32 +21,52 @@
       viewContents = [err];
     }
   });
-  function method() {
-    viewContents.forEach(function(viewcontent, index) {
-      if (index === 0) {
-        if (Array.isArray(viewcontent.key)) {
-          startkey = JSON.stringify(viewcontent.key);
-          endkey = JSON.stringify(viewContents[viewContents.length - 1].key);
-          let options = {
-            group: true,
-            group_level: 1,
-            startkey: startkey,
-            endkey: endkey
-          };
-          let response = view(token, dbname, ddoc, viewname, options);
-        } else {
-          startkey = JSON.stringify(viewcontent.key);
-          endkey = JSON.stringify(viewContents[viewContents.length - 1].key);
-          let options = {
-            group: false,
-            reduce: false,
-            startkey: startkey,
-            endkey: endkey
-          };
-          let response = view(token, dbname, ddoc, viewname, options);
+
+  function reduceView() {
+    var updateKey;
+    var tabRecords = document.getElementById("table");
+    var rows = tabRecords.rows;
+    for (var i = 0; i < rows.length; i++) {
+      rows[i].onclick = function() {
+        if (this.parentNode.nodeName == "THEAD") {
+          return;
         }
-      }
-    });
+        var cells = this.cells;
+        updateKey = document.getElementById("tdata");
+        updateKey.value = cells[0].innerHTML;
+        selectedVal = updateKey.value;
+        viewContents.forEach(function(viewcontent, index) {
+          if (index === 0) {
+            if (Array.isArray(viewcontent.key)) {
+              startkey = JSON.stringify(selectedVal);
+              console.log("start", startkey);
+              endkey = JSON.stringify(
+                viewContents[viewContents.length - 1].key
+              );
+              let options = {
+                group: true,
+                group_level: 1,
+                startkey: startkey,
+                endkey: endkey
+              };
+              let response = view(token, dbname, ddoc, viewname, options);
+            } else {
+              startkey = JSON.stringify(viewcontent.key);
+              endkey = JSON.stringify(
+                viewContents[viewContents.length - 1].key
+              );
+              let options = {
+                group: false,
+                reduce: false,
+                startkey: startkey,
+                endkey: endkey
+              };
+              let response = view(token, dbname, ddoc, viewname, options);
+            }
+          }
+        });
+      };
+    }
   }
 </script>
 
@@ -74,10 +95,7 @@
     </tr>
     {#each viewContents as data}
       <tr>
-        <td on:click={method}>
-          {data.key}
-          <!-- <a href="/coltCollection">{data.key}</a> -->
-        </td>
+        <td id="tdata" on:click={reduceView}>{data.key}</td>
         <td>{data.value}</td>
       </tr>
     {/each}
