51da04ce7f9acc6a9af02330fcf575c12acc5f1b
Sascha Adler <sadler@crkn.ca>
2019-10-15 13:37:21 -0400

    Rewrites to use HTML datalist (who knew?)

--- src/components/TypeAhead.svelte
+++ src/components/TypeAhead.svelte
@@ -2,7 +2,6 @@
   import { onMount, createEventDispatcher } from "svelte";
   import { state as authState } from "../auth.js";
   import { all_docs } from "../couch.js";
-  import Option from "./TypeAhead/Option.svelte";
   const dispatch = createEventDispatcher();
 
   let token = $authState.token;
@@ -10,67 +9,49 @@
   export let db;
   let idFragment = "";
   let results = [];
-  var showResults = true;
   onMount(async () => {
     await lookupIds();
   });
 
   async function lookupIds(event) {
-    if (
-      event &&
-      event.key == "Enter" &&
-      results[0] &&
-      results[0].id == idFragment
-    ) {
-      dispatch("typeahead.idselected", { id: idFragment });
-      showResults = false;
-    } else {
-      dispatch("typeahead.iddeselected");
-      try {
-        results = (await all_docs(token, db, {
-          startkey: JSON.stringify(idFragment),
-          endkey: JSON.stringify(`${idFragment}\uFFEF`),
-          limit: 20
-        })).rows.filter(row => !row.key.startsWith("_design"));
-      } catch (ignore) {
-        results = ["Cannot retrieve results."];
-      }
-      showResults = true;
+    dispatch("typeahead.iddeselected");
+    try {
+      results = (await all_docs(token, db, {
+        startkey: JSON.stringify(idFragment),
+        endkey: JSON.stringify(`${idFragment}\uFFEF`),
+        limit: 12
+      })).rows.filter(row => !row.key.startsWith("_design"));
+    } catch (ignore) {
+      results = ["Cannot retrieve results."];
     }
   }
 
   function selectId(event) {
-    idFragment = event.detail.id;
-    showResults = false;
+    dispatch("typeahead.idselected", { id: idFragment });
   }
 </script>
 
 <style>
-  ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    font-size: 16px;
-  }
   input {
     border: 1px solid var(--brand-color);
+    font-size: 120%;
+    width: 30ch;
+    padding: 10px;
   }
 </style>
 
 <div>
+  <label for="typeaheadInput">Input a document ID:</label>
   <input
     type="text"
+    id="typeaheadInput"
+    list="typeaheadData"
     bind:value={idFragment}
-    on:keyup={lookupIds}
-    placeholder="Search" />
-  {#if showResults}
-    <ul>
-      {#each results as result}
-        <Option
-          id={result.id}
-          on:typeahead.idselected
-          on:typeahead.idselected={selectId} />
-      {/each}
-    </ul>
-  {/if}
+    on:input={lookupIds}
+    on:change={selectId} />
+  <datalist id="typeaheadData">
+    {#each results as result}
+      <option>{result.id}</option>
+    {/each}
+  </datalist>
 </div>
