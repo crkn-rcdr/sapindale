e31edf9cbe9606556d571b6e0e6ea751d65a0463
Russell McOrmond <rmcormond@crkn.ca>
2020-05-18 16:48:48 -0400

    * new version of VSCode seems to format a bit differently * New function * set Content-Type for requests  (@SaschaAdler should review).

--- src/couch.js
+++ src/couch.js
@@ -2,7 +2,7 @@ import qs from "query-string";
 import testManifestData from "./cantaloupe.js";
 
 const upholsteryUrl = process.env.UPHOLSTERY;
-const packagingdatabase = 'wipmetad';
+const packagingdatabase = "wipmetad";
 
 async function _request(token, path, options, method, payload) {
   let url = [upholsteryUrl, path].join("/");
@@ -11,8 +11,9 @@ async function _request(token, path, options, method, payload) {
   let fetchOptions = {
     headers: {
       Authorization: `Bearer ${token}`,
-      Accept: "application/json"
-    }
+      Accept: "application/json",
+      "Content-Type": "appliction/json",
+    },
   };
   if (method) fetchOptions.method = method;
   if (payload) fetchOptions.body = JSON.stringify(payload);
@@ -51,7 +52,7 @@ async function documents(token, db, options) {
     [db, "_all_docs"].join("/"),
     options
   );
-  return result.rows.filter(row => !row.key.startsWith("_design"));
+  return result.rows.filter((row) => !row.key.startsWith("_design"));
 }
 
 async function idLookup(token, db, idList) {
@@ -61,7 +62,7 @@ async function idLookup(token, db, idList) {
     {},
     "POST",
     {
-      keys: idList
+      keys: idList,
     }
   );
   return result;
@@ -71,16 +72,16 @@ async function design_doc_views(token) {
   let views = {};
   await Promise.all(
     dbs
-      .filter(db => db[0] !== "_" && db !== "bin")
-      .map(async db => {
+      .filter((db) => db[0] !== "_" && db !== "bin")
+      .map(async (db) => {
         let ddocs = (
           await _couch_request(token, [db, "_all_docs"].join("/"), {
             startkey: JSON.stringify("_design"),
             endkey: JSON.stringify("_design\uFFEF"),
-            include_docs: true
+            include_docs: true,
           })
         ).rows;
-        ddocs.map(ddoc => {
+        ddocs.map((ddoc) => {
           if (ddoc.doc.views) {
             views[db] = views[db] || {};
             views[db][ddoc.id.substring(8)] = Object.keys(ddoc.doc.views);
@@ -104,7 +105,7 @@ async function view(token, db, ddoc, view, options) {
 async function testCantaloupe(id, ctoken, token) {
   let cvs = await _api_request(token, `${id}`);
   let listItems = cvs.canvases;
-  var generateList = listItems.map(n => {
+  var generateList = listItems.map((n) => {
     let constructPath = {};
     constructPath.id = n.id;
     constructPath.label = n.label;
@@ -119,20 +120,41 @@ async function testCantaloupe(id, ctoken, token) {
   });
   return {
     label: cvs.label,
-    items: generateList
+    items: generateList,
   };
 }
 
-
 async function packagingfilesystem(token, options) {
-
   let result = await _couch_request(
     token,
-    [ packagingdatabase, "_design/tdr/_view/filesystem"].join("/"),
+    [packagingdatabase, "_design/tdr/_view/filesystem"].join("/"),
     options
   );
   return result.rows;
 }
 
+//  Once at a time, waiting for each to return...
+//  Should look into promise-throttle?
+async function packagingrequests(token, aiplist, reqs) {
+  for (const aip of aiplist) {
+    for (const req of reqs) {
+      await _couch_request(
+        token,
+        [packagingdatabase, "_design/tdr/_update/basic", aip].join("/"),
+        {},
+        "POST",
+        req
+      );
+    }
+  }
+}
 
-export { idLookup, documents, design_doc_views, view, testCantaloupe, packagingfilesystem };
+export {
+  idLookup,
+  documents,
+  design_doc_views,
+  view,
+  testCantaloupe,
+  packagingfilesystem,
+  packagingrequests,
+};
