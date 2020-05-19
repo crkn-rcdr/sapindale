c02f3b2e8877c3c9fd6bcb67bbd0caf26533d1b1
Russell McOrmond <rmcormond@crkn.ca>
2020-05-19 10:15:18 -0400

    Renamed tool to generic name.

---
+++ src/routes/packaging.svelte
@@ -0,0 +1,9 @@
+<script>
+  import PackagingStartProcess from "../components/Packaging.svelte";
+</script>
+
+<svelte:head>
+  <title>Packaging</title>
+</svelte:head>
+
+<PackagingStartProcess />
--- src/routes/packagingstartprocess.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import PackagingStartProcess from "../components/PackagingStartProcess.svelte";
-</script>
-
-<svelte:head>
-  <title>Packaging — Start Process</title>
-</svelte:head>
-
-<PackagingStartProcess />
