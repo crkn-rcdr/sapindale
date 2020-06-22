9feb2a32e051c92eff4e390ff7cf44b1a5a9fca2
DivyaKrishnan26 <dkrishnan@crkn.ca>
2020-06-21 20:18:11 -0400

    First version of collection record

--- src/components/CollectionEditor.svelte
+++ src/components/CollectionEditor.svelte
@@ -3,53 +3,106 @@
   import { onMount, createEventDispatcher } from "svelte";
   import SlugResolver from "../components/SlugResolver.svelte";
   import { collectionrequest as request } from "../api/collection.js";
+  import TextValueEditor from "../components/TextValueEditor.svelte";
+  import SortableList from "../components/SortableList.svelte";
+  import CollectionItems from "../components/CollectionItems.svelte";
 
   export let id = undefined;
+  const dispatch = createEventDispatcher();
   let token = $authState.token;
   let rowcount = [];
-  
+  let itemCount = [];
+  let items;
+  let showItems = ["id", "label", "slug", "type"];
+
+  onMount(async () => {
+    await getCollectionRecords({ id });
+  });
+
   async function getCollectionRecords({ id }) {
-    rowcount = await request(token, id);
-    console.log("test", rowcount);
+    try {
+      rowcount = await request(token, id);
+      itemCount = rowcount.items.map(row => row.id);
+      console.log("item", itemCount);
+      console.log("test", rowcount);
+    } catch (ignore) {}
+  }
+  function displayItems(event) {
+    dispatch("select", { itemCount });
+    console.log("show", itemCount);
   }
 </script>
 
-<input type="text" bind:value={id} />
-<button on:click={getCollectionRecords({ id })}>Display Records</button>
-<table>
-  <thead>
-    <tr>
-      <th>Collection Records for: {rowcount.id}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      {#each Object.keys(rowcount) as item}
-        <th>{item}</th>
-      {/each}
-    </tr>
-    <tr>
+<style>
+  .line {
+    display: -webkit-box;
+  }
+  .spacewidth {
+    line-height: 0.5rem;
+  }
+  .scroll {
+    overflow-y: scroll;
+    height: 100vh;
+    width: 50%;
+    background-color: #1d808b15;
+    color: #141010;
+  }
+</style>
+
+{#if rowcount}
+  <ul>
+    <li>
       {#each Object.keys(rowcount) as item}
         {#if typeof rowcount[item] == 'object'}
-          <td>
-            {#each Object.keys(rowcount[item]) as element}
-              {rowcount[item][element]}
-            {/each}
-          </td>
+          <li>
+            {item}:
+            {#if item !== 'items'}
+              {#each Object.keys(rowcount[item]) as element}
+                <span class="line">
+                  <input type="text" bind:value={element} />
+                  <input type="text" bind:value={rowcount[item][element]} />
+                </span>
+              {/each}
+              {#if item === 'label'}
+                <TextValueEditor />
+              {/if}
+            {/if}
+            {#if item === 'items'}
+              <div class="scroll">
+                {#each Object.keys(rowcount[item]) as element}
+                  <ul>
+                    <pre>
+                      {#each Object.keys(rowcount[item][element]) as items}
+                        {#if showItems.includes(items)}
+                          <li class="spacewidth">
+
+                            {#if items !== 'label'}
+                              {items}:{rowcount[item][element][items]}
+                            {:else if items == 'label'}
+                              {#each Object.keys(rowcount[item][element][items]) as labels}
+                                {labels}:{rowcount[item][element][items][labels]}
+                              {/each}
+                            {/if}
+
+                          </li>
+                        {/if}
+                      {/each}
+                    </pre>
+                  </ul>
+                {/each}
+              </div>
+            {/if}
+          </li>
         {:else}
-          <td>{rowcount[item]}</td>
+          <li>{item}:{rowcount[item]}</li>
         {/if}
       {/each}
-    </tr>
-  </tbody>
-</table>
-<div class="ordered">
-  <input type="checkbox" />
-  <p>Ordered?</p>
+
+    </li>
+  </ul>
 
   <div class="buttons">
     <button id="save">Save</button>
     <button id="publish">Save & Publish</button>
   </div>
-
-</div>
+{/if}
--- src/components/TextValueEditor.svelte
+++ src/components/TextValueEditor.svelte
@@ -1,81 +1,53 @@
 <script>
   import FaBackspace from "svelte-icons/fa/FaBackspace.svelte";
   import { state as authState } from "../auth.js";
-  import { collectionrequest } from "../api/collection.js";
+
   let value = "";
   let valueDe,
     valueLang = "";
   let rowcount;
-  let labelinColl = [];
 
-  let id, db;
-
-  let token = $authState.token;
-
-  async function collection() {
-    let prefix = encodeURIComponent("69429/");
-    rowcount = await collectionrequest(token, prefix + id);
-
-    labelinColl = rowcount.label;
-  }
   async function clearText(index) {
     valueDe = "";
     /* This will be taken when Update function is written */
-    let testUpdate = await Object.values(labelinColl)[index];
+    /* let testUpdate = await Object.values(labelinColl)[index]; */
   }
 </script>
 
 <style>
-  table,
-  th,
-  tr,
-  td {
-    border: none;
-  }
   .icon {
     color: red;
     width: 50px;
     height: 50px;
+    font-size: small;
+  }
+  .add {
+    line-height: 1%;
+    font-size: small;
   }
 </style>
 
-<h2>Edit Label</h2>
-<label for="collectionid">Type in the Collection ID:</label>
-<input type="text" bind:value={id} on:blur={collection} />
 <table>
   <tr>
     <th>Language</th>
     <th>Value</th>
   </tr>
-  {#if labelinColl != null}
-    {#each Object.keys(labelinColl) as label, index}
-      <tr>
-        <td>{label}</td>
-        <td>
-          <input type="text" bind:value={labelinColl[label]} />
-        </td>
-        <td>
-          <FaBackspace />
-        </td>
-      </tr>
-    {/each}
-    <tr>
-      <td>
-        <input
-          type="text"
-          placeholder="Add New Language"
-          bind:value={valueLang} />
-      </td>
-      <td>
-        <input
-          type="text"
-          placeholder="Add New Text Value"
-          bind:value={valueDe} />
-      </td>
-      <td class="icon" on:click|preventDefault={clearText}>
-        <FaBackspace />
-      </td>
-    </tr>
-  {/if}
+  <tr>
+    <td>
+      <input
+        type="text"
+        placeholder="Add New Language"
+        bind:value={valueLang} />
+    </td>
+    <td>
+      <input
+        type="text"
+        placeholder="Add New Text Value"
+        bind:value={valueDe} />
+    </td>
+    <td class="icon" on:click|preventDefault={clearText}>Clear Text</td>
+    <td>
+      <button class="add">Add</button>
+    </td>
+  </tr>
 </table>
-<button class="add">Add Label</button>
