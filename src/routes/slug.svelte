7cadd01dce5a51e7e1c47bb7a0a84450278a9101
Sascha Adler <sadler@crkn.ca>
2020-06-24 10:49:16 -0600

    Fixes merge conflict

--- src/routes/slug.svelte
+++ src/routes/slug.svelte
@@ -1,20 +1,10 @@
 <script>
   import SlugTypeAhead from "../components/SlugTypeAhead.svelte";
-<<<<<<< HEAD
-  let id;
-  let prefix, displayValue, displayType;
-  function search(event) {
-    id = event.detail.value;
-  }
-  function clear() {
-    id = undefined;
-=======
   let slug;
   let mode = "manifest";
 
   function selected(event) {
     slug = event.detail;
->>>>>>> master
   }
 
   function clear(event) {
@@ -27,21 +17,8 @@
   }
 </script>
 
-<<<<<<< HEAD
-<h1>Slug Resolver</h1>
-<div>
-  <p>
-    {#if id}Searched slug id: {id}{:else}Search a slug id by typing it.{/if}
-  </p>
-  <SlugResolver
-    on:searched={search}
-    on:deselected={clear}
-    on:selected={displayUrl} />
-</div>
-=======
 <h1>Slug Lookup</h1>
 
->>>>>>> master
 <div>
   <SlugTypeAhead
     bind:mode
