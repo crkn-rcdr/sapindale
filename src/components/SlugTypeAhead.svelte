3942b4e1cacecdb0969ec1b9dd67c1a029f272e8
Sascha Adler <sadler@crkn.ca>
2020-06-24 09:53:33 -0600

    Separates slug lookups into collection and manifest calls. Implements a solution for SlugTypeAhead for switching between the two. I've left SlugResolver looking only at collection slugs. (#71)
    Closes #70.

--- src/components/SlugTypeAhead.svelte
+++ src/components/SlugTypeAhead.svelte
@@ -1,42 +1,71 @@
 <script>
   import { onMount, createEventDispatcher } from "svelte";
   import { state as authState } from "../auth.js";
-  import { search as searchSlug } from "../api/slug.js";
+  import {
+    resolveSlug as resolveCollectionSlug,
+    searchSlug as searchCollectionSlug
+  } from "../api/collection.js";
+  import {
+    resolveSlug as resolveManifestSlug,
+    searchSlug as searchManifestSlug
+  } from "../api/manifest.js";
 
   const dispatch = createEventDispatcher();
+  export let mode;
 
   let prefix = "";
   let lookupList = [];
   let token = $authState.token;
 
   export let label = "Please provide a label for this component.";
-  /*  onMount(async () => {
-    await lookupSlug();
-  }); */
+
+  function clear() {
+    lookupList = [];
+    dispatch("deselected", {});
+  }
+
   async function lookupSlug() {
     try {
-      lookupList = await searchSlug(token, prefix);
+      let searchMethod =
+        mode === "manifest" ? searchManifestSlug : searchCollectionSlug;
+      lookupList = await searchMethod(token, prefix);
     } catch (ignore) {}
   }
   async function selectItem(event) {
-    dispatch("selected", { prefix });
+    let resolveMethod =
+      mode === "manifest" ? resolveManifestSlug : resolveCollectionSlug;
+    let slug = await resolveMethod(token, prefix);
+    dispatch("selected", slug);
   }
 </script>
 
 <style>
   label {
-    padding-top: 4%;
     display: block;
     width: 150px;
     text-align: left;
   }
+
+  input[type="text"],
+  datalist {
+    width: 500px;
+  }
 </style>
+
+<label>
+  <input type="radio" bind:group={mode} on:select={clear} value="manifest" />
+  Manifest
+</label>
+
+<label>
+  <input type="radio" bind:group={mode} on:select={clear} value="collection" />
+  Collection
+</label>
 
 <label for={`${prefix}.input`}>{label}</label>
 <input
   type="text"
   id="{prefix}.input"
-  class="data-list border"
   list={`${lookupList}.prefix`}
   bind:value={prefix}
   on:input={lookupSlug}
