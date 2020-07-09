c5acdc312213ab1cf1b17c95e1b87cb3c5e61878
Sascha Adler <sadler@crkn.ca>
2020-07-09 17:43:30 -0600

    Simplifies TextValueEditor; disables the add new language button unless the code is long enough

--- src/components/TextValueEditor.svelte
+++ src/components/TextValueEditor.svelte
@@ -3,54 +3,38 @@
     mandatory = false,
     textarea = false;
 
-  let label;
-  let labelPresent = false;
+  let newLanguage = "";
+  $: newLanguageDisabled = newLanguage.length < 2 || newLanguage.length > 4;
+  let newLanguageAlreadyExists = false;
 
-  //Updates data object with new entires
-  function input(inputField, Value, keyValue, i) {
-    if (inputField == "language" && (Value != null || Value != "")) {
-      if (Value in data) {
-        labelPresent = true;
-        data = data;
+  // Adds a new entry to data
+  function addEntry() {
+    if (!newLanguageDisabled) {
+      if (Object.keys(data).includes(newLanguage)) {
+        newLanguageAlreadyExists = true;
       } else {
-        data[Value] = data[Object.keys(data)[i]];
-        delete data[Object.keys(data)[i]];
+        data[newLanguage] = "";
+        newLanguage = "";
         data = data;
-      }
-    } else if (inputField == "langValue") {
-      if (Value != null || value != "") {
-        data[keyValue] = Value;
-        data = data;
-      }
-    }
-  }
-  //TO clear and reset entry
-  function clearEntry(label) {
-    if (label != undefined) {
-      if (!(label in data) && label.length < 2) {
-        labelPresent = false;
       }
     }
   }
 
-  // TO clear the row
-  async function removeRecord(i) {
-    delete data[Object.keys(data)[i]];
+  // Clears the warning that appears if a new language already exists
+  function clearWarning() {
+    if (newLanguageAlreadyExists) newLanguageAlreadyExists = false;
+  }
+
+  // Removes an entry
+  function removeEntry(language) {
+    delete data[language];
     data = data;
   }
 </script>
 
 <style>
   .languageInput {
-    width: 4em;
-  }
-  .addnew {
-    background-color: var(--color);
-    text-align: center;
-    color: var(--color-bg);
-  }
-  .exist {
-    color: red;
+    width: 4ch;
   }
 </style>
 
@@ -58,62 +42,48 @@
 
   <tr>
     <th>Language</th>
-    <th>value</th>
+    <th>Value</th>
   </tr>
 
-  {#each Object.keys(data) as textValue, i (i)}
+  {#each Object.keys(data).sort() as language}
     <tr>
       <td>
-        <label for="language">{textValue}</label>
+        <b>{language}</b>
       </td>
       {#if textarea}
         <td>
-          <textarea
-            name="langValue"
-            bind:value={data[textValue]}
-            on:blur={input('langValue', data[textValue], textValue, i)} />
+          <textarea bind:value={data[language]} />
         </td>
       {:else}
         <td>
-          <input
-            name="langValue"
-            type="text"
-            bind:value={data[textValue]}
-            on:blur={input('langValue', data[textValue], textValue, i)} />
+          <input type="text" bind:value={data[language]} />
         </td>
       {/if}
-      {#if (Object.keys(data).length > 1 && mandatory) || textarea}
+      {#if !(Object.keys(data).length < 2 && mandatory)}
         <td>
-          <a href on:click|preventDefault={removeRecord(i)}>Remove Entry</a>
+          <a href on:click|preventDefault={removeEntry(language)}>
+            Remove entry
+          </a>
         </td>
       {/if}
     </tr>
   {/each}
-  <tr>
-    <td>
-
-      <input
-        type="text"
-        name="language"
-        class="languageInput"
-        minlength="2"
-        maxlength="4"
-        bind:value={label}
-        onchange={clearEntry(label)} />
-
-    </td>
-    <td>
-      {#if labelPresent && label in data}
-        <p class="exist">Language Exist</p>
-      {:else if !labelPresent}
-        <a
-          href
-          class="addnew"
-          on:click|preventDefault={input('language', label, '', '')}>
-          Add New Language
-        </a>
-      {/if}
-    </td>
-
-  </tr>
 </table>
+<form class="inline">
+  <input
+    type="text"
+    class="languageInput"
+    minlength="2"
+    maxlength="4"
+    bind:value={newLanguage}
+    on:input={clearWarning} />
+  <button
+    type="submit"
+    on:click|preventDefault={addEntry}
+    disabled={newLanguageDisabled}>
+    Add new language
+  </button>
+  {#if newLanguageAlreadyExists}
+    <span class="danger">There is already an entry for this language.</span>
+  {/if}
+</form>
