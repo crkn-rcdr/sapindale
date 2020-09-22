e81a6eab05250eaee8450c3196a9a44da4e04cca
Russell McOrmond <rmcormond@crkn.ca>
2020-09-22 10:12:54 -0400

    Scaffolding

---
+++ src/components/DMD.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from "svelte";
+  import { stores } from "@sapper/app";
+
+  const { session } = stores();
+  let token = $session.token;
+
+  export let id = undefined;
+</script>
+
+{#if id}ID={id}{:else}Undefined ID{/if}
---
+++ src/routes/dmd/[id].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { id } = page.params;
+    return { id };
+  }
+</script>
+
+<script>
+  import Dmd from "../../components/DMD.svelte";
+  export let id;
+</script>
+
+<svelte:head>
+  <title>Sapindale — Descriptive MetaData loader</title>
+</svelte:head>
+
+<Dmd {id} />
