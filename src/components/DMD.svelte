5e01e098c54dfc0983976b5dbeb8af8f1e245e7b
Russell McOrmond <rmcormond@crkn.ca>
2020-10-09 09:33:50 -0400

    Some additional testing.

--- src/components/DMD.svelte
+++ src/components/DMD.svelte
@@ -57,10 +57,16 @@
     metadatafile = undefined,
     mdname = undefined,
     mydoc = undefined,
-    myattachment = undefined;
+    myattachment = undefined,
+    myitems = undefined;
 
   onMount(async () => {
     await updateLocalFromDoc();
+    if (mydoc) {
+      depositor = mydoc.depositor;
+      mdtype = mydoc.mdtype;
+      mdname = mydoc.mdname;
+    }
   });
 
   async function updateLocalFromDoc() {
@@ -69,15 +75,18 @@
       try {
         thisdoc = await getdoc(token, id);
       } catch (ignore) {}
-      depositor = thisdoc.depositor;
-      mdtype = thisdoc.mdtype;
-      mdname = thisdoc.mdname;
+
       mydoc = thisdoc;
-      if ("_attachments" in mydoc && mdname in mydoc._attachments)
-        myattachment = mydoc._attachments[mdname];
+      console.log(mydoc);
+      myattachment =
+        "_attachments" in mydoc && mdname in mydoc._attachments
+          ? mydoc._attachments[mdname]
+          : undefined;
+      myitems = "items" in mydoc ? mydoc.items : undefined;
     } else {
       mydoc = undefined;
       myattachment = undefined;
+      myitems = undefined;
     }
   }
 
@@ -135,30 +144,37 @@
   }
 </script>
 
-<legend>
-  Select depositor:
-  <select bind:value={depositor} on:blur={updateDepositor}>
-    <option value="" />
-    {#each depositors as thisdepositor}
-      <option value={thisdepositor.id}>{thisdepositor.name}</option>
-    {/each}
-  </select>
-</legend>
-
-<legend>
-  Select type:
-  <select bind:value={mdtype} on:blur={updateMdtype}>
-    <option value="" />
-    <option value="issueinfocsv">Issueinfo CSV</option>
-    <option value="dccsv">Dublin Core CSV</option>
-    <option value="marc490">MARC - ID in 490</option>
-    <option value="marcoocihm">MARC - ID in oocihm interpretation</option>
-    <option value="marcooe">MARC - ID in ooe interpretation</option>
-  </select>
-</legend>
+<style>
+  td.message {
+    word-wrap: break-word;
+  }
+</style>
 
 <fieldset>
   <legend>Metadata file</legend>
+
+  <legend>
+    Select depositor:
+    <select bind:value={depositor} on:blur={updateDepositor}>
+      <option value="" />
+      {#each depositors as thisdepositor}
+        <option value={thisdepositor.id}>{thisdepositor.name}</option>
+      {/each}
+    </select>
+  </legend>
+
+  <legend>
+    Select type:
+    <select bind:value={mdtype} on:blur={updateMdtype}>
+      <option value="" />
+      <option value="issueinfocsv">Issueinfo CSV</option>
+      <option value="dccsv">Dublin Core CSV</option>
+      <option value="marc490">MARC - ID in 490</option>
+      <option value="marcoocihm">MARC - ID in oocihm interpretation</option>
+      <option value="marcooe">MARC - ID in ooe interpretation</option>
+    </select>
+  </legend>
+
   {#if myattachment}
     <table>
       <tr>
@@ -179,18 +195,74 @@
     Upload file:
     <input type="file" id="upload" name="upload" on:change={uploadFile} />
   </legend>
-
+  <p>
+    <button
+      type="submit"
+      on:click={() => {
+        doSplit();
+      }}>
+      Initiate Split
+    </button>
+  </p>
 </fieldset>
 
-<button
-  type="submit"
-  on:click={() => {
-    doSplit();
-  }}>
-  Initiate Split
-</button>
 <br />
+{#if mydoc && 'split' in mydoc}
+  <fieldset>
+    <legend>Split task</legend>
+    <table>
+      {#if 'requestDate' in mydoc.split}
+        <tr>
+          <td>Request Date</td>
+          <td>{mydoc.split.requestDate}</td>
+        </tr>
+      {/if}
+      {#if 'succeeded' in mydoc.split}
+        <tr>
+          <td>Success?</td>
+          <td>{mydoc.split.succeeded ? 'Yes' : 'No'}</td>
+        </tr>
+      {/if}
+      {#if mydoc.split.message !== ''}
+        <tr>
+          <td>Message</td>
+          <td class="message">{mydoc.split.message}</td>
+        </tr>
+      {/if}
+      {#if 'processDate' in mydoc.split}
+        <tr>
+          <td>Process Date</td>
+          <td>{mydoc.split.processDate}</td>
+        </tr>
+      {/if}
+
+    </table>
+  </fieldset>
+{/if}
 <br />
+
+{#if myitems}
+  <fieldset>
+    <legend>Metadata records found</legend>
+    <table>
+      {#each myitems as item}
+        <tr>
+          <td>{item.id ? item.id : '[not found]'}</td>
+          <td>{item.accessidfound ? item.accessidfound : '[not found]'}</td>
+          <td>
+            {item.preservationidfound ? item.preservationidfound : '[not found]'}
+          </td>
+          <td>{item.validated ? 'Yes' : 'No'}</td>
+          <td>{item.message ? item.message : ''}</td>
+          <td>{item.pubmin ? item.pubmin : ''}</td>
+          <td>{item.pubmax ? item.pubmax : ''}</td>
+          <td>Copy2access</td>
+          <td>copy2preservation</td>
+        </tr>
+      {/each}
+    </table>
+  </fieldset>
+{/if}
 <table>
   {#if depositor}
     <tr>
