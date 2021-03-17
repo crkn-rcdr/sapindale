6cf8352e1e392377786909dc306d3e23e5d66b9c
Sascha Adler <sadler@crkn.ca>
2021-03-17 20:00:41 -0230

    Explains what's going on in resources/couch.js, and sets up the skeleton for porting over the repository stats code

--- src/models/repository.js
+++ src/models/repository.js
@@ -1,109 +1,108 @@
-import {
-  repositoryfilesize,
-  repositoryverified,
-  repositoryreplicate,
-} from "../resources/couch";
-let repostats = undefined,
-  verified,
-  replicate = undefined;
+import timespan from "pretty-ms";
+import { getView } from "../resources/couch";
 
-async function repoLoad(merged) {
-  var reposize = undefined;
-  try {
-    var reposize = await repositoryfilesize({
-      group_level: 1,
-    });
-  } catch (ignore) {}
+const db = "tdrepo";
+const ddoc = "tdr";
 
-  if (Array.isArray(reposize)) {
-    var sizetemp = {};
-    reposize.map(async function (arepo) {
-      sizetemp[arepo.key[0]] = arepo.value;
-    });
-    repostats = sizetemp;
-    let repovar = Object.keys(repostats);
-    repovar.map(async function (repository = "") {
-      verified[repository] = {};
-      rows = await repositoryverified({
-        limit: 1,
-        reduce: false,
-        startkey: JSON.stringify([repository]),
-        endkey: JSON.stringify([repository, {}]),
-      }).then(async function (rows) {
-        if (Array.isArray(rows) && rows.length === 1) {
-          verified[repository].earliest = new Date(rows[0].key[1]);
-          calculate_human();
-        }
-        return verified[repository].earliest;
-      });
+const getVerified = async (repo, latest = false) => {
+  // TODO: implement this
+  return new Date();
+};
 
-      await repositoryverified({
-        limit: 1,
-        reduce: false,
-        descending: true,
-        endkey: JSON.stringify([repository]),
-        startkey: JSON.stringify([repository, {}]),
-      }).then(async function (rows) {
-        if (Array.isArray(rows) && rows.length === 1) {
-          verified[repository].latest = new Date(rows[0].key[1]);
-          calculate_human();
-        }
-        return verified[repository].latest;
-      });
+const getReplicate = async (repo) => {
+  // TODO: implement this
+  return 0;
+};
 
-      await repositoryreplicate({
-        group_level: 1,
-      }).then(async function (rows) {
-        if (Array.isArray(rows) && rows.length > 0) {
-          var temp = {};
-          rows.forEach(function (row = {}) {
-            temp[row.key[0]] = row.value;
-          });
-          replicate = temp;
-        }
-        return replicate;
-      });
-    });
-    /* Promise.all(repovar).then(function (response) {
-      console.log("Inside Promise", response);
-    }); */
-    merged = {
-      ...repostats,
-    };
-    console.log("Merged", merged);
+const load = async () => {
+  const response = await getView(db, ddoc, "repofilesize", {
+    group_level: 1,
+  });
+
+  if (response.status === 200) {
+    const stats = await Promise.all(
+      response.content.rows.map(async (row) => {
+        const repo = row.key[0];
+        const earliest = await getVerified(repo, false);
+        const latest = await getVerified(repo, true);
+        const difference = timespan(latest - earliest, { compact: true });
+        const replicate = await getReplicate(repo);
+
+        return {
+          repo,
+          aipCount: row.value.count,
+          diskUsed: row.value.sum,
+          earliest,
+          latest,
+          difference,
+          replicate,
+        };
+      })
+    );
+
+    return stats;
+
+    // let repovar = Object.keys(repostats);
+    // repovar.map(async function (repository = "") {
+    //   verified[repository] = {};
+    //   rows = await getView(db, ddoc, "repoverified", {
+    //     limit: 1,
+    //     reduce: false,
+    //     startkey: JSON.stringify([repository]),
+    //     endkey: JSON.stringify([repository, {}]),
+    //   }).then(async function (rows) {
+    //     if (Array.isArray(rows) && rows.length === 1) {
+    //       verified[repository].earliest = new Date(rows[0].key[1]);
+    //       calculate_human();
+    //     }
+    //     return verified[repository].earliest;
+    //   });
+
+    //   await repositoryverified({
+    //     limit: 1,
+    //     reduce: false,
+    //     descending: true,
+    //     endkey: JSON.stringify([repository]),
+    //     startkey: JSON.stringify([repository, {}]),
+    //   }).then(async function (rows) {
+    //     if (Array.isArray(rows) && rows.length === 1) {
+    //       verified[repository].latest = new Date(rows[0].key[1]);
+    //       calculate_human();
+    //     }
+    //     return verified[repository].latest;
+    //   });
+
+    //   await repositoryreplicate({
+    //     group_level: 1,
+    //   }).then(async function (rows) {
+    //     if (Array.isArray(rows) && rows.length > 0) {
+    //       var temp = {};
+    //       rows.forEach(function (row = {}) {
+    //         temp[row.key[0]] = row.value;
+    //       });
+    //       replicate = temp;
+    //     }
+    //     return replicate;
+    //   });
+    // });
+    // /* Promise.all(repovar).then(function (response) {
+    //   console.log("Inside Promise", response);
+    // }); */
+    // merged = {
+    //   ...repostats,
+    // };
+    // console.log("Merged", merged);
+    // return {
+    //   status: 200,
+    //   content: { merged },
+    // };
+  } else {
     return {
-      status: 200,
-      content: { merged },
+      status: response.status,
+      content: null,
+      error: `repofilesize lookup failed: ${response.message}`,
     };
   }
-}
+};
 
-function calculate_human() {
-  Object.keys(repostats).forEach(function (repository = "") {
-    if (
-      "earliest" in verified[repository] &&
-      "latest" in verified[repository] &&
-      !("human" in verified[repository])
-    ) {
-      var d1 = verified[repository].latest - verified[repository].earliest;
-
-      var diffDays = parseInt(d1 / (1000 * 60 * 60 * 24));
-      var months = Math.floor(diffDays / 30);
-      var days = diffDays - months * 30;
-
-      var mydate = new Date(d1);
-      verified[repository].human =
-        months +
-        " months, " +
-        days +
-        " days, " +
-        mydate.getUTCHours() +
-        " hours, " +
-        mydate.getUTCMinutes() +
-        " minutes and " +
-        mydate.getUTCSeconds() +
-        " second(s)";
-    }
-  });
-}
-export default { repoLoad, calculate_human };
+export default { load };
--- src/routes/repository/repository.json.js
+++ src/routes/repository/repository.json.js
@@ -1,9 +1,6 @@
 import Repository from "../../models/repository";
 
 export async function get(req, res) {
-  const repo = req.params.merged;
-  console.log("hello");
-  const response = await Repository.repoLoad("merged", repo);
-  console.log("---", response);
+  const response = await Repository.load();
   res.finalizeJSON(response);
 }
