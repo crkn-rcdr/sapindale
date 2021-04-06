99ff96ff2c49794b5ddc89930804005eaf5abc2a
Sascha Adler <sadler@crkn.ca>
2021-04-06 17:04:08 -0230

    Distinguishes null slug resolutions from unperformed ones (#122)

--- src/routes/slug/index.svelte
+++ src/routes/slug/index.svelte
@@ -6,6 +6,8 @@
 
 <Resolver slug="oocihm.00001" noid="69429/m0w950g3k03r" hideInitial={true} />
 <Resolver slug="oocihm.00001" noid="69429/m0w950g3k03r" hideInitial={false} />
+<Resolver slug="oocihm.0000" noid={null} hideInitial={true} />
+<Resolver slug="oocihm.0000" noid={null} hideInitial={false} />
 <Resolver slug="oocihm.00001" hideInitial={true} />
 <Resolver slug="oocihm.00001" hideInitial={false} />
 <Resolver slug="oocihm.0000" hideInitial={true} />
