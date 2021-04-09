d48a16ea87b2a06b3f6a982a8497d919ec4eb544
Russell McOrmond <rmcormond@crkn.ca>
2021-04-09 12:10:54 -0400

    Update to send ... err ... update.

--- src/routes/dipstaging/updates.json.js
+++ src/routes/dipstaging/updates.json.js
@@ -1,9 +1,37 @@
+import { updateCouch } from "../../resources/couch";
+
+// Hard coded at top as config seems fine. Part of review
+const dipstagingdatabase = "dipstaging";
 
 export async function post(req, res) {
   let jsonreturn = {};
-  console.log(req.body);
 
-  // Deliberately not here yet.  Get some feedback on test version before adding live code that changes database...
-  
+  function updateBasic(aip = "", update = {}) {
+    return updateCouch(dipstagingdatabase, "sync", "basic", aip, update);
+  }
+
+  // One request at a time to not flood CouchDB.
+  // Someone can later update this to allow a limited number of concurrent requests.
+  for (const aip of Object.keys(req.body)) {
+    const data = req.body[aip];
+    if (typeof data === "object") {
+      switch (data.action) {
+        case "smelt":
+          await updateBasic(aip, {
+            dosmelt: true,
+            slug: data.slug,
+          });
+          break;
+        case "clear":
+          // TODO: A better server-side update should be created which deletes the `smelt` key
+          await updateBasic(aip, {
+            smelt: "{}",
+          });
+          break;
+      }
+    }
+  }
+
+  // We don't currently return anything to indicate success or failure to user.
   res.finalizeJSON(jsonreturn);
 }
